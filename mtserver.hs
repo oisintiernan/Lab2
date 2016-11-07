4a31df0a5530254f1cf7084332826d19edc85496
Oisin Tiernan <tiernano@tcd.ie>
2016-11-07 15:05:46 +0000

    third

--- mtserver.hs
+++ mtserver.hs
@@ -1,12 +1,11 @@
-module Main where
- 
+import System.IO 
 import Network.Socket
- 
+
 main :: IO ()
 main = do
     sock <- socket AF_INET Stream 0    -- create socket
     setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
-    bind sock (SockAddrInet 4242 iNADDR_ANY)   -- listen on TCP port 4242.
+    bind sock (SockAddrInet 8000 0x0100007f)   -- listen on TCP port 8000 address 127.0.0.1
     listen sock 2                              -- set a max of 2 queued connections
     mainLoop sock                              -- unimplemented
 -- in Main.hs
@@ -14,11 +13,15 @@ main = do
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
     conn <- accept sock     -- accept a connection and handle it
+    putStrLn "In Main Loop"
     runConn conn            -- run our server's logic
     mainLoop sock           -- repeat
  
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
-    send sock "Hello!\n"
-    close sock
-Chat conversation end
+    hdl <- socketToHandle sock ReadWriteMode
+    contents <- hGetContents hdl
+    putStrLn (contents)
+    hSetBuffering hdl NoBuffering
+    hPutStrLn hdl "Hello!"
+    hClose hdl
