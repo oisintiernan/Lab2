ea26d5d5d9a2464287344de1b9d6cba9ed1d914b
oisin tiernan <tiernano@tcd.ie>
2016-11-17 17:59:06 +0000

    h

--- mtserver.hs
+++ mtserver.hs
@@ -32,7 +32,7 @@ messaging  sock addr hdl = do
     putStrLn (contents)
     if (contents == "KILL_SERVICE")
         then cls sock hdl
-        else if (contents == "HELO BASE_TEST\n")
+        else if (contents == "HELO BASE_TEST")
             then infoSplit addr hdl
             else hPutStrLn hdl "keep going"
     messaging sock addr hdl
